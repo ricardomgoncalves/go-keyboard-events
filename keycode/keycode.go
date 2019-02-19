c180b6bb3568a4a1f88154b5efcc3daafa6707fb
ricardomgoncalves <ricardo.goncalves@tlantic.com>
2019-02-19 13:42:02 +0000

    added some keycodes

--- keycode/keycode.go
+++ keycode/keycode.go
@@ -8,6 +8,11 @@ const (
 	RightControl = "R_CTRL"
 	LeftAlt      = "L_ALT"
 	RightAlt     = "R_ALT"
+	One          = "1"
+	Two          = "2"
+	Three        = "3"
+	Four         = "4"
+	Five         = "5"
 )
 
 type KeyCode string
